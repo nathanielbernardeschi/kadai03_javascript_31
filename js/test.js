b7825bb9fa6e3cff9e72fdb4fb51447e496d3635
Taylorh <95158605+nathanielbernardeschi@users.noreply.github.com>
2021-12-25 05:56:22 +0900

    Add files via upload

---
+++ js/test.js
@@ -0,0 +1,19 @@
+function previewFile(file) {
+    // プレビュー画像を追加する要素
+    const preview = document.getElementById('preview');
+  
+    // FileReaderオブジェクトを作成
+    const reader = new FileReader();
+  
+    // ファイルが読み込まれたときに実行する
+    reader.onload = function (e) {
+      const imageUrl = e.target.result; // 画像のURLはevent.target.resultで呼び出せる
+      const img = document.createElement("img"); // img要素を作成
+      img.src = imageUrl; // 画像のURLをimg要素にセット
+      preview.appendChild(img); // #previewの中に追加
+    }
+  
+    // ファイルを読み込む
+    reader.readAsDataURL(file);
+  }
+  
